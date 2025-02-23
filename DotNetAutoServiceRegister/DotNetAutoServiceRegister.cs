9c900d5538e7d5fa7a9f6c213cad459d9d32dd05
Lluís Beltrán Serra <llubisky@gmail.com>
2025-02-23 17:52:27 +0100

    Version 0.5 of the project

---
+++ DotNetAutoServiceRegister/DotNetAutoServiceRegister.cs
@@ -0,0 +1,58 @@
+﻿namespace DotNetAutoServiceRegister
+{
+    public class DotNetAutoServiceRegister
+    {
+        /// <summary>
+        /// Enum that defines the ServiceLifetime of the service we want to register
+        /// </summary>
+        public enum AutoServiceLifetime
+        {
+            Singleton,
+            Scoped,
+            Transient
+        }
+
+        /// <summary>
+        /// Attribute that defines a class as a service. It will be registered by default as a Singleton
+        /// </summary>
+        [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+        public class ServiceAttribute : Attribute
+        {
+            public AutoServiceLifetime Lifetime { get; }
+
+            public ServiceAttribute(AutoServiceLifetime lifetime = AutoServiceLifetime.Singleton)
+            {
+                Lifetime = lifetime;
+            }
+        }
+
+        /// <summary>
+        /// Attribute that defines a class as a component. It will be registered by default as a Singleton
+        /// </summary>
+        [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+        public class ComponentAttribute : Attribute
+        {
+            public AutoServiceLifetime Lifetime { get; }
+
+            public ComponentAttribute(AutoServiceLifetime lifetime = AutoServiceLifetime.Singleton)
+            {
+                Lifetime = lifetime;
+            }
+        }
+        /// <summary>
+        /// Attribute that defines a class as a repository. It will be registered by defaul as transient
+        /// </summary>
+        [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+        public class RepositoryAttribute : Attribute
+        {
+            public AutoServiceLifetime Lifetime { get; }
+
+            public RepositoryAttribute(AutoServiceLifetime lifetime = AutoServiceLifetime.Transient)
+            {
+                Lifetime = lifetime;
+
+
+            }
+        }
+    }
+}
---
+++ DotNetAutoServiceRegister/ServiceCollectionExtensions.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+using System.Reflection;
+using static DotNetAutoServiceRegister.DotNetAutoServiceRegister;
+
+namespace DotNetAutoServiceRegister
+{
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Method that will add the decorators to your services
+        /// </summary>
+        /// <param name="services">IServiceCollection implementation</param>
+        /// <param name="assembly">Assembly of the class</param>
+        public static void AddDecoratedServices(this IServiceCollection services, Assembly assembly)
+        {
+            // Shame of me... I dont want to use var but here is the easiest and simplest way to do it
+            var types = assembly.GetTypes()
+                .Where(type => type.IsClass && !type.IsAbstract)
+                .Select(type => new
+                {
+                    Type = type,
+                    ServiceAttribute = type.GetCustomAttribute<ServiceAttribute>(),
+                    ComponentAttribute = type.GetCustomAttribute<ComponentAttribute>(),
+                    RepositoryAttribute = type.GetCustomAttribute<RepositoryAttribute>()
+                });
+            // As each type in the collection is anonymous I will continue with my shame using var...
+            foreach (var typeInfo in types)
+            {
+                if (typeInfo.ServiceAttribute != null)
+                {
+                    RegisterService(services, typeInfo.Type, typeInfo.ServiceAttribute.Lifetime);
+                }
+                else if (typeInfo.ComponentAttribute != null)
+                {
+                    RegisterService(services, typeInfo.Type, typeInfo.ComponentAttribute.Lifetime);
+                }
+                else if (typeInfo.RepositoryAttribute != null)
+                {
+                    RegisterService(services, typeInfo.Type, typeInfo.RepositoryAttribute.Lifetime);
+                }
+            }
+        }
+        /// <summary>
+        /// Method that will register the service in the IServiceCollection
+        /// </summary>
+        /// <param name="services">IServiceCollection implementation</param>
+        /// <param name="type">Type that we want to register</param>
+        /// <param name="lifetime">LifeTime cicle for the service</param>
+        private static void RegisterService(IServiceCollection services, Type type, AutoServiceLifetime lifetime)
+        {
+            Type[]? interfaces = type.GetInterfaces();
+
+            foreach (Type? @interface in interfaces)
+            {
+                switch (lifetime)
+                {
+                    case DotNetAutoServiceRegister.AutoServiceLifetime.Singleton:
+                        services.AddSingleton(@interface, type);
+                        break;
+                    case DotNetAutoServiceRegister.AutoServiceLifetime.Scoped:
+                        services.AddScoped(@interface, type);
+                        break;
+                    case DotNetAutoServiceRegister.AutoServiceLifetime.Transient:
+                        services.AddTransient(@interface, type);
+                        break;
+                }
+            }
+        }
+    }
+}
